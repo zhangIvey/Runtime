6384978bae31991b141c29ac32aca84bdfaa5456
张泽 <zhangzeStrong@126.com>
2017-05-12 11:19:11 +0800

    项目初始化

---
+++ Runtime/Runtime/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Runtime
+//
+//  Created by yaoln on 2017/5/12.
+//  Copyright © 2017年 zhangze. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Runtime/Runtime/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Runtime
+//
+//  Created by yaoln on 2017/5/12.
+//  Copyright © 2017年 zhangze. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
